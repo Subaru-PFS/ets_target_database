b9b17af08f3869332f90c1976398a11f0f03b3e5
Masato Onodera <monodera@naoj.org>
2021-09-27 17:43:23 -1000

    .ini file is made for the postgres server information in the example

--- examples/scripts/test_targetdb_api.py
+++ examples/scripts/test_targetdb_api.py
@@ -1,65 +1,90 @@
 #!/usr/bin/env python
 
+import configparser
 import datetime
 
 import pandas as pd
+from targetdb import models
 from targetdb import targetdb
 
+# pd.options.display.max_columns = None
 
-def main():
-    hostname = "localhost"
-    port = 15432
-    dbname = "targetdb"
-    username = "admin"
-    password = "admin"
 
-    db = targetdb.TargetDB(
-        hostname=hostname,
-        port=port,
-        dbname=dbname,
-        username=username,
-        passwd=password,
-    )
+def connect_db(conf=None):
+
+    config = configparser.ConfigParser()
+    config.read(conf)
+
+    print(dict(config["database"]))
+
+    db = targetdb.TargetDB(**dict(config["database"]))
+
     db.connect()
 
+    return db
+
+
+def insert_proposal_category(db):
+    df_proposal_category = pd.read_csv("../data/proposal_category.csv")
+    n_proposal_category = len(df_proposal_category.index)
+
+    try:
+        print("trying to insert data into proposal_category...")
+
+        utcnow = datetime.datetime.utcnow()
+        df_proposal_category["created_at"] = [utcnow] * n_proposal_category
+        df_proposal_category["updated_at"] = [utcnow] * n_proposal_category
+
+        db.insert("proposal_category", df_proposal_category)
+    except:
+        print("no unique proposal_category_name is found. skip.")
+
+    return db
+
+
+def insert_proposal(db):
+    df_proposal = pd.read_csv("../data/proposal.csv")
+    n_proposal = len(df_proposal.index)
+
     res = db.fetch_all("proposal_category")
+    df_joined = df_proposal.merge(
+        res,
+        how="left",
+        left_on="proposal_category_name",
+        right_on="proposal_category_name",
+    )
+    print(df_joined)
+
+    try:
+        print("trying to insert data into proposal...")
+
+        utcnow = datetime.datetime.utcnow()
+        df_joined["created_at"] = [utcnow] * n_proposal
+        df_joined["updated_at"] = [utcnow] * n_proposal
+
+        db.insert("proposal", df_joined)
+    except:
+        print("no unique proposal_id is found. skip.")
+
+    res = db.fetch_all("proposal")
     print(res)
+
+    return db
+
+
+def main(conf=None):
+
+    db = connect_db(conf)
 
     # db.reset()
-    # res = db.fetch_all("proposal_category")
-    # print(res)
 
-    if res.empty:
-        utcnow = datetime.datetime.utcnow()
-        df = pd.DataFrame(
-            {
-                "proposal_category_name": ["openuse", "keck", "gemini", "uh"],
-                "proposal_category_description": [
-                    "Subaru openuse proposals",
-                    "Subaru-Keck time exchange proposals",
-                    "Subaru-Gemini time exchange proposals",
-                    "University of Hawaii proposals",
-                ],
-                "created_at": [utcnow, utcnow, utcnow, utcnow],
-                "updated_at": [utcnow, utcnow, utcnow, utcnow],
-            }
-        )
-        db.insert("proposal_category", df)
-        res = db.fetch_all("proposal_category")
-        print(res)
+    # db = insert_proposal_category(db)
 
-    # # this should make error as 'proposal_category_name` must be unique.
-    # db.insert("proposal_category", df)
-    # res = db.fetch_all("proposal_category")
-    # print(res)
-
-    res = db.fetch_query(
-        "SELECT proposal_category_id FROM proposal_category WHERE proposal_category_name='uh';"
-    )
-    print(res)
+    # db = insert_proposal(db)
 
     db.close()
 
 
 if __name__ == "__main__":
-    main()
+    conf = "targetdb_config.ini"
+    main(conf)
--- setup.py
+++ setup.py
@@ -99,7 +99,8 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 2 - Pre-Alpha",
+        # "Development Status :: 3 - Alpha",
         # Indicate who your project is intended for
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
