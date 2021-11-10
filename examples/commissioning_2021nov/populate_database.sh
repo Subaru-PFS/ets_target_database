bd53f09fc89a49830c16fd9dafc17cc72da1322f
Masato Onodera <monodera@naoj.org>
2021-11-10 12:20:21 -1000

    update to populate targetdb with the star list v0.3

--- examples/commissioning_2021nov/populate_database.sh
+++ examples/commissioning_2021nov/populate_database.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-DB_CONF_FILE="targetdb_config_pfsa-db01-gb.ini"
+DB_CONF_FILE="../../../database_configs/targetdb_config_pfsa-db01-gb.ini"
 PY_SCRIPT="test_targetdb_api_comm_2021nov.py"
+STARDATA_DIR="../../../../star_catalogs_ishigaki/"
+STARDATA_PREFIX="target_fstars_v0.3_s21b-en16"
 
 python ./${PY_SCRIPT} ${DB_CONF_FILE} --reset --skip_target
 
-for target in data/target_fstars_s21b-en16-0000??.feather; do
+for target in ${STARDATA_DIR}/${STARDATA_PREFIX}*.feather; do
     python ./${PY_SCRIPT} ${DB_CONF_FILE} \
         --skip_proposal_category \
         --skip_proposal \
