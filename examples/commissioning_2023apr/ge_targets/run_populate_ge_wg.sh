12ee55de933c4ccf3f7335ffed422b160958a8c9
Kiyoto Yabe <kiyoyabe@naoj.org>
2023-05-10 12:52:15 -1000

    add new emission line targets

--- examples/commissioning_2023apr/ge_targets/run_populate_ge_wg.sh
+++ examples/commissioning_2023apr/ge_targets/run_populate_ge_wg.sh
@@ -7,7 +7,7 @@ PY_SCRIPT="targetdb_api_utils.py"
 DATADIR="/work/pfs/commissioning/2023apr/commissioning/data/ge_targets"
 CONFFILE="/work/pfs/commissioning/2023apr/commissioning/configs/config.toml"
 
-#DRYRUN="--dry_run"
+DRYRUN="--dry_run"
 DRYRUN=""
 
 # # Populate input catalogs
@@ -30,7 +30,39 @@ DRYRUN=""
 #    --infile ${DATADIR}/ge_run11_wg_en1.csv \
 #    --input_catalog "hscssp_pdr3_dud"
 
-python ./append_ge_wg_targets_additional_2023apr.py \
+#python ./append_ge_wg_targets_additional_2023apr.py \
+#    ${CONFFILE} ${DRYRUN} \
+#    --infile ${DATADIR}/RQG_y_brightest.cat \
+#    --input_catalog "hscssp_pdr3_dud"
+
+DATADIR="/work/pfs/commissioning/2023apr/commissioning/data"
+
+#python ./append_ge_specz_targets_2023apr.py \
+#    ${CONFFILE} ${DRYRUN} \
+#    --infile ${DATADIR}/hsc_pdr3_dud_en1_418030.csv \
+#    --input_catalog "hscssp_pdr3_dud" \
+#    --proposal_id "S22B-EN16-CO"
+
+#python ./append_ge_photometric_targets_2023apr.py \
+#    ${CONFFILE} ${DRYRUN} \
+#    --infile ${DATADIR}/hsc_pdr3_dud_cos_418033.csv \
+#    --input_catalog "hscssp_pdr3_dud" \
+#    --proposal_id "S22B-EN16-CO"
+
+#python ./append_ge_photometric_targets_2023apr.py \
+#    ${CONFFILE} ${DRYRUN} \
+#    --infile ${DATADIR}/hsc_pdr3_dud_en1_418032.csv \
+#    --input_catalog "hscssp_pdr3_dud" \
+#    --proposal_id "S22B-EN16-CO"
+
+#python ./append_ge_emission_targets_2023apr.py \
+#    ${CONFFILE} ${DRYRUN} \
+#    --infile ${DATADIR}/hsc_pdr3_dud_en1_418054.csv \
+#    --input_catalog "hscssp_pdr3_dud" \
+#    --proposal_id "S22B-EN16-CO"
+
+python ./append_ge_specz_deep_targets_2023apr.py \
     ${CONFFILE} ${DRYRUN} \
-    --infile ${DATADIR}/RQG_y_brightest.cat \
-    --input_catalog "hscssp_pdr3_dud"
+    --infile ${DATADIR}/hsc_pdr3_dud_en1_418030.csv \
+    --input_catalog "hscssp_pdr3_dud" \
+    --proposal_id "S22B-EN16-CO"
