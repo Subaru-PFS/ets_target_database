8a3cb197a3feb218b6b523a28892b4f920a1b722
Masato Onodera <monodera@naoj.org>
2024-04-22 16:54:27 -1000

    documentation update and alembic script

--- alembic/pfsa-db01-gb/alembic/env.py
+++ alembic/pfsa-db01-gb/alembic/env.py
@@ -1,7 +1,6 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 from targetdb import models
 
 from alembic import context
@@ -45,6 +44,7 @@ def run_migrations_offline():
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        # compare_server_default=True,
     )
 
     with context.begin_transaction():
@@ -65,7 +65,11 @@ def run_migrations_online():
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            # compare_server_default=True,
+        )
 
         with context.begin_transaction():
             context.run_migrations()
